b62fef1fee3f82414aa09eadbe61599cd547809e
Yuri <112394310+ltsmeyuri@users.noreply.github.com>
2022-11-17 09:48:26 -0300

    Update questão 1.py

--- questão 1.py
+++ questão 1.py
@@ -1,18 +1,16 @@
-lista1 = []
-lista2 = []
-lista3 = []
-lista4 = []
-
-while True:
-    x = (int(input("Digite um número: "))) 
-    if 25>=x>=0:
-        lista1.append(x)
-    elif 50>=x>=26:
-        lista2.append(x)
-    elif 75>=x>=51:
-        lista3.append(x)
-    elif 100>=x>=76:
-        lista4.append(x)
-    if x < 0:
-        break
-print('Os números entre 0-25 são {}, entre 26-50 são {}, entre 51-75 são {} e entre 76-100 são {}.'.format(lista1, lista2, lista3, lista4))
+a=b=c=d=n=0
+while n>=0:
+    n = float(input('Insira um número: '))
+    if 0<=n<=25: 
+        a+=1
+    elif 26<=n<=50:
+        b+=1
+    elif 51<=n<=75:
+        c+=1
+    elif 76<=n<=100:
+        d+=1
+    elif n>100:
+        print('Valor inválido.')
+    else:
+        print('Valor negativo. Fim da contagem.')
+print ('A quantidade de números inseridos nos intervalos:\n',f'[0-25] = {a}.\n', f'[26-50] = {b}.\n', f'[51-75] = {c}.\n', f'[76-100] = {d}.\n')
